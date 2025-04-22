464e4100f2def802fc01480b27ef8abdef3c0318
Abdulaziz A Mohammed <62390035+Mekhlafi98@users.noreply.github.com>
2025-04-23 00:04:54 +0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH = True
 
-import os
 
 GEMINI_API_KEY = os.environ['GOOGLE_API_KEY']
 
