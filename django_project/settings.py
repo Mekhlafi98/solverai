3f7ab79dff0f79d49ee0d0a47a99063e3e61f98d
Abdulaziz A Mohammed <62390035+Mekhlafi98@users.noreply.github.com>
2025-04-23 00:56:27 +0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -167,15 +167,15 @@ LOGGING = {
     },
 }
 
-# from django.middleware.clickjacking import XFrameOptionsMiddleware
+from django.middleware.clickjacking import XFrameOptionsMiddleware
 
-# class CustomXFrameOptionsMiddleware(XFrameOptionsMiddleware):
-#     def process_response(self, request, response):
-#         response['X-Frame-Options'] = 'ALLOW-FROM https://azeez98-solveai.hf.space'
-#         return response
+class CustomXFrameOptionsMiddleware(XFrameOptionsMiddleware):
+    def process_response(self, request, response):
+        response['X-Frame-Options'] = 'ALLOW-FROM https://azeez98-solveai.hf.space'
+        return response
 
-# MIDDLEWARE.insert(
-#     MIDDLEWARE.index('django.middleware.clickjacking.XFrameOptionsMiddleware'),
-#     'django_project.settings.CustomXFrameOptionsMiddleware'
-# )
-# MIDDLEWARE.remove('django.middleware.clickjacking.XFrameOptionsMiddleware')
+MIDDLEWARE.insert(
+    MIDDLEWARE.index('django.middleware.clickjacking.XFrameOptionsMiddleware'),
+    'django_project.settings.CustomXFrameOptionsMiddleware'
+)
+MIDDLEWARE.remove('django.middleware.clickjacking.XFrameOptionsMiddleware')
